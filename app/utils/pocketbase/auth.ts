dd06d80f7e95b9894acebd2a597629485739f2f7
EurikaOrmanel <wworgba@gmail.com>
2025-05-08 23:50:39 +0000

    auth

--- app/utils/pocketbase/auth.ts
+++ app/utils/pocketbase/auth.ts
@@ -3,9 +3,7 @@ import PocketBase from "pocketbase";
 const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKET_BASE_URL!)
 
 export async function loginUser(email: string, password: string) {
-    console.log("pppppppppppppp:", process.env.NEXT_PUBLIC_POCKET_BASE_URL);
     const data = await pb.collection("users").authWithPassword(email, password);
-    console.log("User logged in:", data);
     return data
 }
 
